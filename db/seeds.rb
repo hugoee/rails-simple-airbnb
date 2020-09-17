d31867edbbb7f350a2aafa68fe8bea19c9958708
Hugo Santos <hugoee@gmail.com>
2020-09-17 17:18:28 -0300

    First commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+
+4.times do
+  Flat.create!(
+    name: Faker::Creature::Animal.name,
+    address: Faker::Address.full_address,
+    description: Faker::Marketing.buzzwords,
+    price_per_night: Faker::Number.decimal(l_digits: 3, r_digits: 2),
+    number_of_guests: rand(1...6)
+  )
+end
+
+puts "Finished!"
+
